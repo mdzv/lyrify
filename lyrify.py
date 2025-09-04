76952eecba2bccfedbace986330f93ce8a02e789
mdzv <mkdzvprojects@gmail.com>
2025-09-04 19:52:54 +0400

    Update lyrify.py

--- lyrify.py
+++ lyrify.py
@@ -3,18 +3,16 @@ import requests
 def GetLyrics(artist, title):
     artist = artist.strip().lower()
     title = title.strip().lower()
-    
     url = f"https://api.lyrics.ovh/v1/{artist}/{title}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         return data.get("lyrics", None)
-    else:
-        return None
-    
+    return None
 
 if __name__ == "__main__":
-    while True:
+    program = True
+    while program:
         artist = input("Enter artist name: ")
         title = input("Enter song title: ")
         
@@ -23,10 +21,10 @@ if __name__ == "__main__":
         if lyrics:
             print("\n Lyrics \n")
             print(lyrics)
-            break
+            program = False 
         else:
             print(f"\nLyrics cannot be found for: {artist} - {title}")
             retry = input("Do you want to try again? (y/n): ").strip().lower()
-            if retry != "y":
+            if retry == "n":
                 print("Exiting program.")
-                break
+                program = False 
